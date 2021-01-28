050e0869fd5933372bdeb80a6c70f41e9c4fcfcb
Cristian "SknZ" Ferreira <61399406+Cristian-Sknz@users.noreply.github.com>
2021-01-27 23:56:41 -0300

    [Feature & Refactor] Correções de bugs e Novo Injetor de dependência. - Console Log4J colorido - Adicionado sistema de injeção de dependência. - SQLBasic tem interação com o sistema de injeção. - Sistema de linguagem modificado: Sistema de linguagem funcionará com Resource Bundle externo. - Algumas correções de bugs e mal funcionamento.

---
+++ src/main/java/me/skiincraft/discord/core/language/UTF8BundleControl.java
@@ -0,0 +1,41 @@
+package me.skiincraft.discord.core.language;
+
+import java.io.*;
+import java.io.InputStream;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ResourceBundle.Control;
+import java.util.*;
+
+public class UTF8BundleControl extends Control {
+
+    public ResourceBundle newBundle (String baseName, Locale locale, String format, ClassLoader loader, boolean reload)
+            throws IOException {
+        String bundleName = toBundleName(baseName, locale);
+        String resourceName = toResourceName(bundleName, "properties");
+        ResourceBundle bundle = null;
+        InputStream stream = null;
+        if (reload) {
+            URL url = loader.getResource(resourceName);
+            if (url != null) {
+                URLConnection connection = url.openConnection();
+                if (connection != null) {
+                    connection.setUseCaches(false);
+                    stream = connection.getInputStream();
+                }
+            }
+        } else {
+            stream = loader.getResourceAsStream(resourceName);
+        }
+        if (stream != null) {
+            try {
+                // Read properties files as user defined and if not provided then use "UTF-8" encoding.
+                String encodingScheme = "UTF-8";
+                bundle = new PropertyResourceBundle(new InputStreamReader(stream, encodingScheme));
+            } finally {
+                stream.close();
+            }
+        }
+        return bundle;
+    }
+}
